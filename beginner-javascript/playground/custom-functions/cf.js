f342cc694f0f6dbcb7fa204b4d1a7547bcff299f
Adam Ponting <adam.ponting@gmail.com>
2020-06-04 13:24:42 +0100

    done 14

--- beginner-javascript/playground/custom-functions/cf.js
+++ beginner-javascript/playground/custom-functions/cf.js
@@ -1,10 +1,30 @@
-function calculateBill() {
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
   console.log('Running calculateBill');
-  const total = 100 * 1.13;
-  //   console.log(total);
+  console.log(billAmount, taxRate);
+  const total = billAmount + billAmount * taxRate + billAmount * tipRate;
   return total;
 }
-const myTotal = calculateBill();
-console.log(`Your total is $${myTotal}`);
 
-console.log(`Your total is $${calculateBill() - '2'}`);
+// const wesTotal = 500;
+// const wexTaxRate = 0.1;
+// const myTotal = calculateBill(wesTotal, wexTaxRate);
+// // console.log(myTotal);
+
+// function sayHiTo(firstName) {
+//   return `Hello ${firstName}`;
+// }
+// const greeting = sayHiTo('Wes');
+// console.log(greeting);
+
+// const kait = 201;
+// const myTotal3 = calculateBill(kait + 10, 0.15);
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+function yell(name = 'Silly Goose') {
+  return `HEY ${name.toUpperCase()}`;
+}
+// console.log(yell(doctorize('aram')));
+
+const myBill4 = calculateBill(100, undefined, 0.5);
