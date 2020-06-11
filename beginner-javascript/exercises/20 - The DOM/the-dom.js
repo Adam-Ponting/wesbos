c32c0de2e661e65eff90584716610c73a13b6adb
Adam Ponting <adam.ponting@gmail.com>
2020-06-11 16:23:27 +0100

    done 23

--- beginner-javascript/exercises/20 - The DOM/the-dom.js
+++ beginner-javascript/exercises/20 - The DOM/the-dom.js
@@ -1,16 +1,27 @@
-// const heading = document.querySelector('h2');
-// // console.log(heading.textContent);
-// // console.log(heading.innerText);
-// // // heading.textContent = 'Adam is cool';
-// // console.dir(heading);
-// // console.log(heading.textContent);
-// console.log(heading.innerHTML);
-// console.log(heading.outerHTML);
+// // const heading = document.querySelector('h2');
+// // // console.log(heading.textContent);
+// // // console.log(heading.innerText);
+// // // // heading.textContent = 'Adam is cool';
+// // // console.dir(heading);
+// // // console.log(heading.textContent);
+// // console.log(heading.innerHTML);
+// // console.log(heading.outerHTML);
 
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent);
-pizzaList.textContent = `${pizzaList.textContent} 🍕`;
-pizzaList.insertAdjacentText('beforeend', '🍕 🍕 🍕 🍕 🍕');
-pizzaList.insertAdjacentText('afterbegin', '🍕 🍕 🍕 🍕 🍕');
-pizzaList.insertAdjacentText('beforebegin', '😊');
-pizzaList.insertAdjacentText('afterend', '😊');
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
+// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+// pizzaList.insertAdjacentText('beforeend', '🍕 🍕 🍕 🍕 🍕');
+// pizzaList.insertAdjacentText('afterbegin', '🍕 🍕 🍕 🍕 🍕');
+// pizzaList.insertAdjacentText('beforebegin', '😊');
+// pizzaList.insertAdjacentText('afterend', '😊');
+
+const pic = document.querySelector('.nice');
+pic.classList.add('round');
+pic.classList.remove('cool');
+pic.classList.toggle('round');
+console.log(pic.classList);
+
+function toggleRound() {
+  pic.classList.toggle('round');
+}
+pic.addEventListener('click', toggleRound);
