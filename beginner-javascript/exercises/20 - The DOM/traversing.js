2d9c108faca23557dfb657d0e10e5faf4a02d0f9
Adam Ponting <adam.ponting@gmail.com>
2020-06-17 10:44:28 +0100

    done 27

---
+++ beginner-javascript/exercises/20 - The DOM/traversing.js
@@ -0,0 +1,13 @@
+// const wes = document.querySelector('.wes');
+// console.log(wes.children);
+// console.log(wes.firstElementChild);
+// console.log(wes.lastElementChild);
+// console.log(wes.previousElementSibling);
+// console.log(wes.nextElementSibling);
+// console.log(wes.parentElement);
+
+const p = document.createElement('p');
+p.textContent = 'I will be removed';
+document.body.append(p);
+p.remove();
+console.log(p);
